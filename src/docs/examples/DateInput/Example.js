13b201ea3da44d2cbeb38490693dde48e65219d5
Andrew Boza <andrew.m.boza@gmail.com>
2018-08-26 20:07:38 -0400

    Fixes build

--- src/docs/examples/DateInput/Example.js
+++ src/docs/examples/DateInput/Example.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import DateInput from 'mas-components/lib/DateInput';
+import DateInput from 'ps-react-virtuaboza/lib/DateInput';
 
 export default function Example() {
   return <DateInput value="2018-08-26" />;
